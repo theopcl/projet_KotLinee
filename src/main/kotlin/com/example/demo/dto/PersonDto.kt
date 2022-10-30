23032fc468e034209b7239a135f0f451b7a2484d
Olivier Capuozzo <olivier.capuozzo@gmail.com>
2022-10-30 09:03:07 +0100

    update PersonDto

--- src/main/kotlin/com/example/demo/dto/PersonDto.kt
+++ src/main/kotlin/com/example/demo/dto/PersonDto.kt
@@ -1,8 +1,25 @@
 package com.example.demo.dto
 
+import javax.validation.constraints.NotBlank
+import javax.validation.constraints.NotNull
+import javax.validation.constraints.Size
+
 data class PersonDto(
-    val firstname: String,
-    val lastname: String,
-    val description: String?,
-    var id: Long? = null
+
+    @field:NotBlank
+    @Size(min=3, max=15)
+    val login: String = "",
+
+    @field:NotBlank
+    @field:Size(min=3, max=30)
+    val firstname: String = "",
+
+    @field:NotBlank
+    @field:Size(min=3, max=30)
+    val lastname: String = "",
+
+    @field:Size(min=0, max=500)
+    val description: String? = null,
+
+    val id: Long? = null
 )
