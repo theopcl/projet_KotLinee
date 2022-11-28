91234d1048725d194b981fcf9b476f23a429cc13
Anoxo91 <anthonydantaspaiva@gmail.com>
2022-11-28 09:53:29 +0100

    3eme commit

--- src/main/kotlin/com/example/demo/repository/PersonRepository.kt
+++
@@ -1,9 +0,0 @@
-package com.example.demo.repository
-
-import com.example.demo.domain.Person
-import org.springframework.data.repository.CrudRepository
-
-interface PersonRepository : CrudRepository<Person, Long> {
-    fun findByLogin(login: String): Person?
-}
-
