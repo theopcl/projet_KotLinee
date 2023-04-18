05ba5c38be4f3cca9219cd761f0fa9b586c78858
Anthony <anthonydantaspaiva@gmail.com>
2023-04-18 11:09:50 +0200

    testIsFeetInchesEqualCmWithPercentage & testGetAgeWithPercentage

--- src/main/kotlin/com/example/demo/service/FunVerif.kt
+++ src/main/kotlin/com/example/demo/service/FunVerif.kt
@@ -11,15 +11,21 @@ import java.time.Period
  * @return 'true' si la personne est âgée de 18 à 88 ans, 'false' sinon
  */
 fun getAge(dateNaissance: String): Boolean {
-    // Extraction du mois, du jour et de l'année de la chaîne dateNaissance
+    // Utilisation de la méthode 'split' pour diviser la chaîne dateNaissance en plusieurs éléments
+    // en utilisant le caractère '/' comme séparateur.
+    // Les éléments sont ensuite stockés dans les variables 'month', 'day' et 'year'.
     val (month, day, year) = dateNaissance.split("/")
 
-    // Calcul de l'âge en années
+
+
+    //    Les variables 'year', 'month' et 'day' sont converties en entier avec 'toInt()' et passées à la méthode 'of'.
+    // 3. Utilisation de la méthode 'between()' de la classe 'Period' pour calculer la période entre
+    //    la date de naissance et la date actuelle.
+    // 4. Accès à la propriété 'years' de l'objet 'Period' pour obtenir l'âge en années.
     val age = Period.between(
         LocalDate.of(year.toInt(), month.toInt(), day.toInt()),
         LocalDate.now()
     ).years
-
     // Retourne 'true' si l'âge est compris entre 18 et 88 ans, sinon 'false'
     return age in 18..88
 }
@@ -32,12 +38,27 @@ fun getAge(dateNaissance: String): Boolean {
  * @return 'true' si les deux mesures sont approximativement égales, 'false' sinon
  */
 fun isFeetInchesEqualCm(feetInches: String, centimeters: String): Boolean {
-    // Extraction des pieds et pouces de la chaîne feetInches, suppression du dernier caractère et conversion en entier
-    val (feet, inches) = feetInches.split(" ").map { it.dropLast(1).toInt() }
+    // Utilisation de la méthode 'split' pour diviser la chaîne 'feetInches' en deux éléments
+    // en utilisant le caractère ' ' (espace) comme séparateur.
+    // Puis, la méthode 'map' est utilisée pour transformer chaque élément de la liste.
+    val (feet, inches) = feetInches.split(" ").map {
+        // Utilisation de 'it' pour faire référence à l'élément courant de la liste.
+        // La méthode 'dropLast' est utilisée pour supprimer le dernier caractère de la chaîne 'it'.
+        // Enfin, la chaîne résultante est convertie en entier avec 'toInt()'.
+        it.dropLast(1).toInt()  }
 
     // Conversion des pieds et pouces en centimètres
     val feetInchesToCm = (feet * 30.48) + (inches * 2.54)
 
     // Retourne 'true' si la différence entre les deux mesures est inférieure à 1,25 cm, sinon 'false'
     return abs(feetInchesToCm - centimeters.toDouble()) < 1.25
+}
+fun main(vehicle :String) {
+
+    var vehicule = vehicle
+    var delimiter = " "
+
+    val parts = vehicule.split(delimiter)
+
+    print(parts)
 }
